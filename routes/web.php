1d8abef6f8b562127b49e4e8cdc7b1f9137a96ae
nisa <nisa10222161@sttcipasung.ac.id>
2025-01-16 16:23:31 +0700

    bikin home page

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,31 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmployerController;
-use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\MyApplicationController;
 use App\Http\Controllers\MyJobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('', function () {
-    return to_route('jobs.index');
-});
+Route::resource('', HomeController::class)->only('index') ;
+// Route::get('', function () {
+//     return to_route('jobs.index');
+// });
 
-Route::resource('jobs', JobController::class)->only(['index', 'show']);
+// Route::resource('jobs', JobController::class)->only(['index', 'show']);
 
-Route::get('login', fn() => to_route('auth.create'))->name('login');
-Route::resource('auth', AuthController::class)->only(['create', 'store']);
-Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
-Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
+// Route::get('login', fn() => to_route('auth.create'))->name('login');
+// Route::resource('auth', AuthController::class)->only(['create', 'store']);
+// Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
+// Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
 
-Route::middleware('auth')->group(function () {
-    Route::resource('jobs.applications', JobApplicationController::class)->only(['create', 'store']);
+// Route::middleware('auth')->group(function () {
+//     Route::resource('jobs.applications', JobApplicationController::class)->only(['create', 'store']);
 
-    Route::resource('my-applications', MyApplicationController::class)->only(['index', 'show', 'destroy']);
+//     Route::resource('my-applications', MyApplicationController::class)->only(['index', 'show', 'destroy']);
 
-    Route::resource('employer', EmployerController::class)->only(['create', 'store']);
+//     Route::resource('employer', EmployerController::class)->only(['create', 'store']);
 
-    Route::middleware('employer')->resource('my-jobs', MyJobController::class);
-});
+//     Route::middleware('employer')->resource('my-jobs', MyJobController::class);
+// });
